44ae3d82ac2cd1091168570cac43cdd79d349a8a
cdt <cdt.eth@gmail.com>
2025-03-19 16:28:30 -0400

    rm constants

--- src/constants.ts
+++ src/constants.ts
@@ -10,12 +10,8 @@ export const HOMEBREW_INSTALL_COMMAND =
 export const NASH_LOCAL_SERVER_PORT = 6274;
 export const NASH_LOCAL_SERVER_CHAT_ENDPOINT = `http://localhost:${NASH_LOCAL_SERVER_PORT}/v1/chat/completions/stream`;
 export const NASH_LOCAL_SERVER_SUMMARIZE_ENDPOINT = `http://localhost:${NASH_LOCAL_SERVER_PORT}/v1/chat/summarize`;
-export const NASH_LOCAL_SERVER_MCP_ENDPOINT = `http://localhost:${NASH_LOCAL_SERVER_PORT}/v1/mcp`;
-export const NASH_LOCAL_SERVER_MCP_CALL_TOOL_ENDPOINT = `${NASH_LOCAL_SERVER_MCP_ENDPOINT}/call_tool`;
 
 export const TERMS_OF_SERVICE_URL = "https://1mcp.ai/terms-of-service.md";
-
-export const FUNCTION_CALL_MARKER = "<f";
 
 export const NASH_LOCAL_SERVER_PATH = `~/Library/Application\\ Support/Nash/nash-local-server-${NASH_LOCAL_SERVER_VERSION}`;
 export const NASH_LOCAL_SERVER_RUN_COMMAND = `cd ${NASH_LOCAL_SERVER_PATH} && .venv/bin/poetry run llm_server`;
