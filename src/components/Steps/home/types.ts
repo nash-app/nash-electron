ae3fd127001eb77c126447bd327f4ff632acc265
cdt <cdt.eth@gmail.com>
2025-03-19 23:46:19 -0400

    add errors to banner

--- src/components/Steps/home/types.ts
+++ src/components/Steps/home/types.ts
@@ -38,10 +38,17 @@ export interface ProviderModel {
 }
 
 export interface ConfigAlert {
-  type: "error" | "warning";
+  type: "error";
   message: string;
   link?: {
     text: string;
     step: SetupStep;
+  };
+  id?: string;
+  dismissible?: boolean;
+  timeout?: number;
+  action?: {
+    text: string;
+    onClick: () => void;
   };
 }
